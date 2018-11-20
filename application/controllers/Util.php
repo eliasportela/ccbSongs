9651a248d7adcfda64fd6cd5975d565f5e99483b
Elias Douglas <edwportela@gmail.com>
2018-11-20 17:29:49 -0200

    edwp_get

--- application/controllers/Util.php
+++ application/controllers/Util.php
@@ -10,12 +10,30 @@ class Util extends CI_Controller {
 		require_once(APPPATH.'libraries/simple_html_dom.php');
 
 		$cd_inicio = 3000;
-		$cd_fim = 3005;
+		$cd_fim = 3000;
 
 		for ($i=$cd_inicio; $i <= $cd_fim; $i++) {
 
-			$page = 'https://www.canticosccb.com.br/cds/'.$i;
+			$page = 'http://www.canticosccb.com.br/cds/'.$i.'/songs';
 			
+	        $service_url     = $page;
+
+	        $curl            = curl_init($service_url);
+	        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	        curl_setopt($curl, CURLOPT_POST, false);
+	        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	        $curl_response   = curl_exec($curl);
+	        curl_close($curl);
+
+	        $json_objekat    = json_decode($curl_response);
+	        //$quotes          = $json_objekat->contents->quotes;
+			
+			var_dump($json_objekat);
+
+
+			/*/
+			$page = 'https://www.canticosccb.com.br/cds/'.$i;
 			if ($html = file_get_html($page)) {	
 				echo "CD: " . $i;
 				echo "<br>";
@@ -36,9 +54,9 @@ class Util extends CI_Controller {
 				echo "CD: ".$i." não encontrado";
 				echo "<br>";
 			}
-
+			/*/
 		}
-		echo "Teste";
+
 	}
 
 }
